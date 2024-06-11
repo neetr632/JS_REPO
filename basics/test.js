974c32d1adcbe618e1eca4455a4440beb1349ca6
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-06-11 06:57:30 +0000

    variables files added to the dict

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,11 @@
-console.log("heelp")
+const accountID = 23423
+let accountEmail = "neeraj@example.com"
+var accountPassword = "12fdefw13"
+accountCity = "New Delhi" 
+
+accountEmail = "new@gmail.com"
+accountPassword = "uwefh928"
+let accountState;
+accountCity = "Jaipur"
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all code as the js code as the newer version
+// alert(3 + 3) //we are usign the nodejs, not browser
+
+console.log(456+43)
+console.log("ytty")
+
+// number => 2 to power of 53, bigint , string => " ", boolean => false/true , null => standalone value , undefined => , symbol => unique
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
